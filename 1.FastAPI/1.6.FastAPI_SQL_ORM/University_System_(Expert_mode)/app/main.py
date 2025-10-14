2d33659d072f7b314f07484ca7c88e84faa74591
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-10-14 20:52:43 +0330

    Add part 6

--- 1.FastAPI/1.6.FastAPI_SQL_ORM/University_System_(Expert_mode)/app/main.py
+++ 1.FastAPI/1.6.FastAPI_SQL_ORM/University_System_(Expert_mode)/app/main.py
@@ -9,17 +9,32 @@ Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
-    yield db
-    db.close()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def read_root():
     return {"Message: ": "Welcome to my university system!"}
 
 @app.post("/student")
-def user_add(firstname: str, lastname: str, average: float, graduated: bool, db: Session= Depends(get_db)):
+def student_create(firstname: str, lastname: str, average: float, graduated: bool, db: Session= Depends(get_db)):
     student = create_student(db, firstname, lastname, average, graduated)
     return student
 
+@app.get("/student")
+def student_read(student_id: int, db: Session= Depends(get_db)):
+    student = read_student(db, student_id)
+    return student
 
+@app.put("/student")
+def student_update(student_id: int, firstname: str= None, lastname: str= None,
+                    average: float= None, graduated: bool= None, db: Session= Depends(get_db)):
+    student = update_student(db, student_id, firstname, lastname, average, graduated)
+    return student
 
+@app.delete("/student")
+def student_delete(student_id: int, db: Session= Depends(get_db)):
+    student = delete_student(db, student_id)
+    return student
