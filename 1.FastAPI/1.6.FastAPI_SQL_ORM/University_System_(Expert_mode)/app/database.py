d454f142948d05f0962dc7eeecbb5120800dc273
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-10-20 00:02:08 +0330

    Add part 7

--- 1.FastAPI/1.6.FastAPI_SQL_ORM/University_System_(Expert_mode)/app/database.py
+++ 1.FastAPI/1.6.FastAPI_SQL_ORM/University_System_(Expert_mode)/app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import  create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://moein_user:moein_pass@localhost:5432/university_db"
-SQLALCHEMY_DATABASE_URL = "postgresql://root:y8w83I4IKoudOQgchcA4Lydv@moein-moatali:5432/postgres" #Liara
+SQLALCHEMY_DATABASE_URL = "postgresql://moein_user:moein_pass@localhost:5432/university_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://root:mSSxhvY2dhRxuRlhp8gxW1AT@blissful-benz-rhwdgxian-db:5432/postgres" #Liara
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
