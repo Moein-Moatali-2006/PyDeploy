e2f0dcc7ea9b3cf07fc6ed4223dc554091218b12
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-10-23 21:05:23 +0330

    Add rhyme_finder main.py

--- 1.FastAPI/1.7.FastAPI_Async/Async_API/main.py
+++ 1.FastAPI/1.7.FastAPI_Async/Async_API/main.py
@@ -1,9 +1,25 @@
+import os
+import dotenv
+import json
 import asyncio
-
 import requests
-from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.get("/")
-async def 
+dotenv.load_dotenv()
+
+async def rhyme_finder(word: str=None):
+    try:
+        API_KEY = os.getenv("API_KEY_RHYME_FINDER")
+        url = f"https://rhyming.ir/api/rhyme-finder?api={API_KEY}&w={word}&sb=1&mfe=2&eq=1"
+        response = requests.request("GET", url)
+        result = json.loads(response.text)
+        for i in result["data_items"]:
+            print(i)
+    except:
+        print("Error")
+
+
+# asyncio.run(rhyme_finder("جان"))
+
+
+
