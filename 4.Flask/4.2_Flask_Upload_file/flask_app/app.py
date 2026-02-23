16ccaa9e1d3ba4504752234f2c367f1011194b64
= <=>
2026-02-24 02:01:42 +0330

    Add YOLO and Data Validation

--- 4.Flask/4.2_Flask_Upload_file/flask_app/app.py
+++ 4.Flask/4.2_Flask_Upload_file/flask_app/app.py
@@ -1,21 +1,50 @@
 import os
+import uuid
+import cv2
+from pydantic import BaseModel
 from werkzeug.utils import secure_filename
-from deepface import DeepFace
+from ultralytics import YOLO
 from flask import Flask, render_template, request, redirect, url_for
 from sqlmodel import SQLModel, Field, Session ,create_engine
 
 # Database
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
     email: str 
+    username: str
+    age: int
+    city: str
+    country: str
     password: str
 
 engine = create_engine("sqlite:///database.db")
 SQLModel.metadata.create_all(engine)
 
+# Data Validation
+class UserModel(BaseModel):
+    first_name: str
+    last_name: str
+    email: str 
+    username: str
+    age: int
+    city: str
+    country: str
+    password: str
+
+
 # Routes
 app = Flask("Face Analysis App")
-app.config["UPLOADS"] = "../uploads/"
+
+app.config["UPLOADS"] = "static/uploads"
+app.config["RESULT_FOLDER"] = "static/results"
+
+os.makedirs(app.config["UPLOADS"], exist_ok=True)
+os.makedirs(app.config["RESULT_FOLDER"], exist_ok=True)
+
+# Model
+model = YOLO("yolo26n.pt")  # load an official model
 
 # index
 @app.route("/")
@@ -26,10 +55,17 @@ def root():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
         email = request.form.get("email")
+        username = request.form.get("username")
+        age = request.form.get("age")
+        city = request.form.get("city")
+        country = request.form.get("country")
         password = request.form.get("password")
 
-        user = User(email=email, password=password)
+        user_valid = UserModel(first_name, last_name, email, username, age, city, country, password)
+        user = User(first_name, last_name, email, username, age, city, country, password)
         with Session(engine) as session:
             session.add(user)
             session.commit()
@@ -41,33 +77,50 @@ def login():
 # upload
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
-    global saved_path
 
     if request.method == "POST":
         file = request.files.get("input_file")
+        if not file or file.filename == "":
+            return "No file selected"
 
         filename = secure_filename(file.filename)
         saved_path = os.path.join(app.config["UPLOADS"], filename)
         file.save(saved_path)
 
-        return redirect(url_for("result"))
+        return redirect(url_for("result", filename=filename))
 
     return render_template("upload.html")
 
 # Result
-@app.route("/result")
-def result():
-    info = DeepFace.analyze(
-    img_path=saved_path,
-    actions=["age"],
-    enforce_detection=False)
+@app.route("/result/<filename>")
+def result(filename):
+    saved_path = os.path.join(app.config["UPLOADS"], filename)
 
-    if isinstance(info, list):
-        age = info[0]["age"]
-    else:
-        age = info["age"]
+    results = model(saved_path)
+    img = cv2.imread(saved_path)
 
-    return render_template("result.html", age=age)
+    for res in results:
+        boxes = res.boxes
+        
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            conf = float(box.conf[0])
+            cls_id = int(box.cls[0])
+            class_name = model.names[cls_id]
+
+            label = f"{class_name} {conf:.2f}"
+
+            if conf > 0.2:
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0,165,255), 2)
+                (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
+                cv2.rectangle(img, (x1, y1 - h - 10), (x1 + w, y1), (0,165,255), -1)
+                cv2.putText(img, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,0), 2)
+
+    result_filename = f"{uuid.uuid4().hex}.png"
+    result_path = os.path.join(app.config["RESULT_FOLDER"], result_filename)
+    cv2.imwrite(result_path, img)
+
+    return render_template("result.html", image=result_filename)
 
 # BMR
 @app.route("/bmr", methods=["GET", "POST"])
